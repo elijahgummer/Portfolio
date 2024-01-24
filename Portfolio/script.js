e0634f942f74d804cad675053dae9f034efce2de
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-24 16:29:51 +1000

    changed the styling for the responsive hamburger to show and not the close x mark

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,9 @@
 var sidemenu = document.getElementById("sidemenu");
 
-function toggleMenu() {
-  if (window.innerWidth <= 600) {
-    sidemenu.classList.toggle("active");
-  } else {
-    // Handle the logic for larger screens, if needed
-  }
-}
+      function openmenu() {
+        sidemenu.classList.add("active");
+      }
+
+      function closemenu() {
+        sidemenu.classList.remove("active");
+      }
