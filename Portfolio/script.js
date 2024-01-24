a56e365c3c119cff9a0608fd4e372875b55169ee
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-24 17:16:48 +1000

    change the side menu

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,11 +1,11 @@
 var sidemenu = document.getElementById("sidemenu");
 var closeIcon = document.getElementById("closeIcon");
 
-      function openmenu() {
-        closeIcon.style.display = "block";
-        console.log("hello");
-      }
 
-      function closemenu() {
-        closeIcon.style.display = "none";
-      }
+function openmenu() {
+  sidemenu.classList.add("open-menu");
+}
+
+function closemenu() {
+  sidemenu.classList.remove("open-menu");
+}
