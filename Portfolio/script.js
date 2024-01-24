947ff07cc370b7e281d7060964777e1024a063b3
Elijah Gummer <elijahgummer5@gmail.com>
2024-01-24 16:37:43 +1000

    made it that when the hamburger icon is clicked the close icon shows

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,7 +1,9 @@
 var sidemenu = document.getElementById("sidemenu");
+var closeIcon = document.getElementById("closeIcon");
 
       function openmenu() {
-        sidemenu.classList.add("active");
+        closeIcon.style.display = "block";
+        console.log("hello");
       }
 
       function closemenu() {
