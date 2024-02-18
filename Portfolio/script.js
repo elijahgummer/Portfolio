63fb47806d53873602c6cd71ab3b0348a6c98c25
Elijah Gummer <elijahgummer5@gmail.com>
2024-02-18 10:13:01 +1000

    changed the projects

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,12 +1,25 @@
 const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      
-    }
-    cancelBtn.onclick = ()=>{
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+};
+cancelBtn.onclick = () => {
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+
+const imageContainers = document.querySelectorAll(
+  "#projects .container .content-container .card .image-container"
+);
+
+imageContainers.forEach((container) => {
+  const img = container.querySelector("img");
+  container.addEventListener("mouseenter", () => {
+    img.style.transform = "translateY(-100%)";
+  });
+  container.addEventListener("mouseleave", () => {
+    img.style.transform = "translateY(0)";
+  });
+});
